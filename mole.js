adf68f4a5edf68b088dea0f2c73882c232e37aa3
David Mejia <david7mejia@gmail.com>
2021-05-05 13:10:42 -0500

    not complete

--- mole.js
+++ mole.js
@@ -1,10 +1,27 @@
-// window.addEventListener('DOMContentLoaded', () => {
+window.addEventListener('DOMContentLoaded', setTimeout(() => {
 
-//     setInterval(() => {
-//       const moleHeads = document.querySelectorAll('.wgs__mole-head');
-//       for (let moleHead of moleHeads) {
-//         moleHead.classList.toggle('wgs__mole-head--hidden');
-//       }
-//     }, 1000);
+    function popUpRandomMole() {
+        const moles = document.querySelectorAll('wgs__mole-head');
+        let randNum = Math.random() * 8;
 
-//   });
+        for (let i = randNum; i <= moles.length; i++) {
+            let mole = moles[i];
+            return mole
+        }
+
+        setTimeout(() => {
+            return hideMole(moles)
+        }, 1000)
+
+    }
+
+    function hideMole(el) {
+        const moleMod = el.forEach(ele => {
+            ele.classList.add('wgs__mole-head--hidden')
+        });
+        setTimeout(() => {
+            return popUpRandomMole(moleMod)
+        }, 1000)
+    }
+}, 0)
+)
