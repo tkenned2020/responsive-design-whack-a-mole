bb2a744039e489cc198104558b48dedef6432c4d
David Mejia <david7mejia@gmail.com>
2021-05-05 15:21:25 -0500

    finished

--- mole.js
+++ mole.js
@@ -1,24 +1,45 @@
 function popUpRandomMole() {
-  const moles = document.querySelectorAll(".wgs__mole-head");
-  let randNum = Math.floor(Math.random() * moles.length),
-    molesIdx = moles[randNum];
 
-  molesIdx.classList.remove("wgs__mole-head--hidden");
 
-  setTimeout(() => {
-    hideMole(molesIdx);
-  }, 1000);
+    const moles = document.querySelectorAll(".wgs__mole-head:not(.wgs__mole-head--whacked)");
+    let randNum = Math.floor(Math.random() * moles.length),
+        molesIdx = moles[randNum];
+
+    if (moles.length === 0) {
+        console.log(`YOU WON!`)
+        return
+    }
+
+    molesIdx.classList.remove("wgs__mole-head--hidden");
+    // molesIdx.classList.remove("wgs__mole-head":not(".wgs__mole - head--whacked"))
+
+    setTimeout(() => {
+        hideMole(molesIdx);
+    }, 3000);
 }
 
 function hideMole(mole) {
-  mole.classList.add("wgs__mole-head--hidden");
-  setTimeout(() => {
-    popUpRandomMole();
-  }, 1000);
+    mole.classList.add("wgs__mole-head--hidden");
+    setTimeout(() => {
+        popUpRandomMole();
+    }, 300);
 }
 
 window.addEventListener("DOMContentLoaded", () => {
-  setTimeout(() => {
-    popUpRandomMole();
-  }, 0);
+    let moles1 = document.querySelectorAll('.wgs__mole-head');
+
+    moles1.forEach(el => {
+        el.addEventListener('click', e => {
+            e.target.classList.add("wgs__mole-head--hidden");
+            e.target.classList.add("wgs__mole-head--whacked");
+
+
+        })
+    })
+
+    setTimeout(() => {
+        popUpRandomMole();
+    }, 0);
+
+
 });
